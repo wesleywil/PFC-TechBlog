460c75caafcd9641b74a6ce9a126e72fc9a006db
wesleywil <wesleywilsonti@gmail.com>
2022-09-17 15:35:57 -0300

    adicionado foto para posts

--- postagens/forms.py
+++ postagens/forms.py
@@ -6,12 +6,14 @@ class PostagemModelForm(forms.ModelForm):
     class Meta:
         model = Postagem
         fields = (
+            'foto',
             'titulo',
             'texto',
             'categorias',
         )
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['foto'].widget.attrs.update({'class':'w-full border-2 bg-white/40  text-xl m-2 rounded-xl pl-1'})
         self.fields['titulo'].widget.attrs.update({'class':'w-full border-2 bg-white/40  text-xl m-2 rounded-xl pl-1'})
         self.fields['texto'].widget.attrs.update({'class':'w-full border-2 bg-white/40  text-xl m-2 rounded-xl pl-1'})
         self.fields['categorias'].widget.attrs.update({'class':'w-full border-2 bg-white/40  text-xl m-2 rounded-xl pl-1'})
--- postagens/models.py
+++ postagens/models.py
@@ -1,9 +1,11 @@
-from tabnanny import verbose
 from django.db import models
 from contas.models import Perfil
 
+from PIL import Image
+
 class Postagem(models.Model):
     autor = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to="postagens/", default="postagens/predefinido/default.png")
     titulo = models.CharField(max_length=100)
     texto = models.TextField()
     data_adicao = models.DateField(auto_now_add=True)
@@ -12,6 +14,16 @@ class Postagem(models.Model):
     class Meta:
         verbose_name = "postagem"
         verbose_name_plural = "postagens"
+
+    def save(self, *args, **kwargs):
+        super().save() # salva a imagem primeiro
+
+        img = Image.open(self.foto.path) # Abrindo imagem usando SELF
+
+        if img.height > 400 or img.width > 800:
+            nova_foto = (1200,800)
+            img.thumbnail(nova_foto)
+            img.save(self.foto.path) # Salva nova imagem no mesmo local
 
     def __str__(self):
         return self.titulo
